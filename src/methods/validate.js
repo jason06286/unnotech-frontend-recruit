b1e8f73bd56ddbeb716b5d72c06a973e582a60e9
jason06286 <dj4871114@gmail.com>
2022-10-16 20:47:51 +0800

    add: dist

--- src/methods/validate.js
+++
@@ -1,8 +0,0 @@
-import * as Yup from 'yup';
-
-const bookSchema = Yup.object().shape({
-  title: Yup.string().required('書名不得為空，請重新輸入'),
-  author: Yup.string().required('作者不得為空，請重新輸入'),
-});
-
-export { bookSchema };
